b5803b3ce09e078f7d0751ef6e4c7e31190aeb46
Daniel Walker <walkerda2010@gmail.com>
2014-11-05 09:40:07 +0000

    setup the ui-router and some partials

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,26 @@
  * Main module of the application.
  */
 angular
-  .module('lessonPlanBuilderApp', ['ui.router']);
+  .module('lessonPlanBuilderApp', ['ui.router'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: 'views/partials/home.html'
+        })
+        .state('contact', {
+          url: '/contact',
+          templateUrl: '/views/partials/contact.html'
+        })
+        .state('tools-lpb', {
+          url: '/tools-lpb',
+          templateUrl: '/views/partials/tools-lpb.html'
+        })
+        .state('blog', {
+          url: '/blog',
+          templateUrl: '/views/partials/blog.html'
+        })
+
+  });
